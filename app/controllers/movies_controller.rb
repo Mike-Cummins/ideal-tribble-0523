e32acc08d9a04ff6bf27013c53fbdba102e02731
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-01-02 09:11:47 -0800

    Complete user story 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
+    @actors = @movie.actors.sort_by_age
   end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -25,11 +25,9 @@ RSpec.describe 'Movies Show' do
     it 'Displays all of the actors listed from youngest to oldest' do 
       visit "/movies/#{@movie_1.id}"
 
-      within "#actors" do
-        expect(@actor_3.name).to appear_before(@actor_1.name)
-        expect(@actor_1.name).to appear_before(@actor_2.name)
-        expect(page).to_not have_content(@actor_4.name)
-      end
+      expect(@actor_3.name).to appear_before(@actor_1.name)
+      expect(@actor_1.name).to appear_before(@actor_2.name)
+      expect(page).to_not have_content(@actor_4.name)
     end
   end
 end
